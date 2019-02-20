d90f0f32180082283cfddbae0b18a48cacbe3762
Tao Bror Bojlén <tao@btao.org>
2019-02-20 17:14:28 +0000

    don't serve static files

--- backend/api/backend/settings/base.py
+++ backend/api/backend/settings/base.py
@@ -120,8 +120,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../../frontend/build/static')
-]
+STATICFILES_DIRS = []
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
