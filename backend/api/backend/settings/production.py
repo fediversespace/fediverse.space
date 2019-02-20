5079875697f879e6c21035c7e0d50626ce7b733e
Tao Bror Bojlén <tao@btao.org>
2019-02-20 20:44:27 +0000

    fix CORS for netlify branch deployments

--- backend/api/backend/settings/production.py
+++ backend/api/backend/settings/production.py
@@ -6,5 +6,6 @@ ALLOWED_HOSTS = ['api.fediverse.space']
 
 CORS_ORIGIN_WHITELIST = [
     'fediverse.space',
-    'staging.fediverse.space'
+    'staging.fediverse.space',
+    '*fediverse-space.netlify.com'
 ]
