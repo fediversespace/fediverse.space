bb2f39e78582fb1378216f823b28c5ce8923e18f
Tao Bror Bojlén <tao@btao.org>
2019-02-20 17:15:41 +0000

    add subdomains to allowed hosts

--- backend/api/backend/settings/production.py
+++ backend/api/backend/settings/production.py
@@ -2,8 +2,10 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['fediverse.space']
+ALLOWED_HOSTS = ['fediverse.space', 'www.fediverse.space', 'staging.fediverse.space']
 
 CORS_ORIGIN_WHITELIST = [
     'fediverse.space',
+    'www.fediverse.space',
+    'staging.fediverse.space'
 ]
