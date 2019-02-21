de1cee2015ac872f7cb866e9a8532df19eff4c7c
Tao Bror Bojlén <tao@btao.org>
2019-02-21 14:38:23 +0000

    add www subdomain to CORS whitelist

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -6,6 +6,7 @@ ALLOWED_HOSTS = ['api.fediverse.space']
 
 CORS_ORIGIN_WHITELIST = [
     'fediverse.space',
+    'www.fediverse.space',
     'staging.fediverse.space',
 ]
 CORS_ORIGIN_REGEX_WHITELIST = (r'^(https?:\/\/)?(\w+\.)?(.*)?fediverse-space\.netlify\.com\/?$', )
