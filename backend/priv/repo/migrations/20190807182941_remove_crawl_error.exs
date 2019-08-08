186abbd9cf1f97e2eafc7f93f1a8d344d0845499
Tao Bror Bojlén <tao@btao.org>
2019-08-08 12:44:29 +0300

    more performant deletes

--- backend/priv/repo/migrations/20190807182941_remove_crawl_error.exs
+++ backend/priv/repo/migrations/20190807182941_remove_crawl_error.exs
@@ -2,7 +2,9 @@ defmodule Backend.Repo.Migrations.RemoveCrawlError do
   use Ecto.Migration
 
   def change do
+    execute("ALTER TABLE crawls DISABLE TRIGGER ALL", "ALTER TABLE crawls ENABLE TRIGGER ALL")
     execute("DELETE FROM crawls WHERE error IS NOT NULL", "")
+    execute("ALTER TABLE crawls ENABLE TRIGGER ALL", "")
 
     alter table(:crawls) do
       remove :error, :string
