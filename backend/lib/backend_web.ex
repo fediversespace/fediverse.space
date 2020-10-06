3148578a7d90fbd721b0921e4014a7a4f93e16a5
Tao Bojlén <2803708-tao_oat@users.noreply.gitlab.com>
2020-10-06 18:06:44 +0100

    fix appsignal compilation errors by reverting to 1.x

--- backend/lib/backend_web.ex
+++ backend/lib/backend_web.ex
@@ -33,8 +33,6 @@ defmodule BackendWeb do
         root: "lib/backend_web/templates",
         namespace: BackendWeb
 
-      use Appsignal.Phoenix.View
-
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
 
